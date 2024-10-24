0eff206ab1ea158806d99541bba76333bc5457e6
Ryan Guimarães <ryan.guimaraes@inchurch.com.br>
2024-10-24 15:01:25 -0300

    fix: handle Unauthorized exception
    This will fail for other exceptions than BadRequest, NotFound and Unauthorized. New exceptions should be added to the list. This is due to application thrown exceptions, only these are handled. Runtime exceptions are handled by the `except` code below.

--- cinema/decorators.py
+++ cinema/decorators.py
@@ -1,4 +1,4 @@
-from tastypie.exceptions import BadRequest, NotFound
+from tastypie.exceptions import BadRequest, NotFound, Unauthorized
 import tastypie.http as http
 
 
@@ -6,8 +6,19 @@ def handle_exceptions(view_func):
     def _wrapped_view(self, request, *args, **kwargs):
         try:
             return view_func(self, request, *args, **kwargs)
-        except (BadRequest, NotFound) as e:
-            return self.create_response(request, {'error': str(e)}, response_class=http.HttpBadRequest if isinstance(e, BadRequest) else http.HttpNotFound)
+        except (BadRequest, NotFound, Unauthorized) as e:
+            return self.create_response(
+                request, {'error': str(e)}, 
+                response_class={
+                    BadRequest: http.HttpBadRequest,
+                    NotFound: http.HttpNotFound,
+                    Unauthorized: http.HttpUnauthorized
+                }[e.__class__]
+            )
         except Exception as e:
-            return self.create_response(request, {'error': 'An unexpected error occurred: %s' % str(e)}, response_class=http.HttpApplicationError)
+            return self.create_response(
+                request, 
+                {'error': 'An unexpected error occurred: %s' % str(e)}, 
+                response_class=http.HttpApplicationError
+            )
     return _wrapped_view
